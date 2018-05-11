808d8c74e6e0ca798d95b3630b44e46a5ecda45e
aseed9 <media@MacBook-Pro-toutes-media.local>
2018-05-12 02:39:50 +0300

    zxaxax

--- bgrab.py
+++ bgrab.py
@@ -1,18 +1,26 @@
 #!usr/bin/env python
 
 # banner.py
+def retBanner(ip, port):
+	import socket
+	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #TCP
 
-import socket
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #TCP
-
-t_host = str(raw_input("Enter the host name: "))
-t_port = int(raw_input("Enter port : "))
-
-sock.connect((t_host,t_port))
+	sock.connect(ip,port)
 
 	
-sock.send('GET HTTP/1.1 \r\n')
-
-ret = sock.recv(1024)
-print 'Scanning...'
-print 'Grabbing 192.168.189.159:' + t_port + '[+]' + str(ret)
+	sock.send('GET HTTP/1.1 \r\n')
+	ret = sock.recv(1024)
+	print 'Scanning...'
+	print 'Grabbing 192.168.189.159:' + t_port + '[+]' + str(ret)
+	return ret
+def main():
+	portList = [21,22,25,80,110,443]
+	for x in range(189,190):
+		banner = retBanner(ip, port)
+		if banner:
+			print '[+]' + ip + ':' + port + '- ' banner
+		else :
+			banner = 'No banner recieved'
+			print '[+]' + ip + ':' + port + '- ' banner 
+		
+	

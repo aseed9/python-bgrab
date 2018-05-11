1a53f0623c74228812f4082575c7889ec0ee78d5
aseed9 <media@MacBook-Pro-toutes-media.local>
2018-05-12 02:27:14 +0300

    xz`z`x``xx

--- bgrab.py
+++ bgrab.py
@@ -6,17 +6,13 @@ import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #TCP
 
 t_host = str(raw_input("Enter the host name: "))
-t_port = int(raw_input("Specify port range start : "))
-tt_port = int(raw_input("Specify port range stop : "))
-portList = range(t_port,tt_port)
+t_port = int(raw_input("Enter port : "))
 
-sock.connect((t_host,portList))
+sock.connect((t_host,t_port))
 
-
-for port in portList:
 	
-	sock.send('GET HTTP/1.1 \r\n')
+sock.send('GET HTTP/1.1 \r\n')
 
-	ret = sock.recv(1024)
-	print 'Scanning ports 1-1024'
-	print 'Grabbing 192.168.189.159' + '[+]' + str(ret)
+ret = sock.recv(1024)
+print 'Scanning...'
+print 'Grabbing 192.168.189.159:' + t_port + '[+]' + str(ret)
